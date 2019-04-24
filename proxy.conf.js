55fa1a873a1c73d17de68cd83325b559ce771e01
Mahesh Sasidharan <msasidharan@cogint.com>
2019-04-24 14:58:04 -0700

    Removing URL

--- proxy.conf.js
+++ proxy.conf.js
@@ -3,10 +3,10 @@ const PROXY_CONFIG = [
         context: [
             "/api"
         ],
-        target: "http://admin.local.idicore.com",
+        target: "http://admin.local.mywebsite.com",
         secure: false,
         pathRewrite: {
-            "^/api": "http://admin.local.idicore.com/api"
+            "^/api": "http://admin.local.mywebsite.com/api"
         },
         changeOrigin: true
     }
